85a9271dc1ad5b074f629ca68cbeb647a7de9d9c
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-01-26 13:16:59 +0200

    JSX render elements

---
+++ lesson2/task3/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const renderSeconds = () => {
+  const rootElement = document.querySelector('#root');
+  const seconds = new Date().getSeconds();
+
+  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
+  const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
+
+  const styles = {
+    color: textColor,
+    background: backgroundColor,
+  };
+
+  const element = (
+    <div className="seconds" style={styles}>
+      Now is {seconds}
+    </div>
+  );
+
+  ReactDOM.render(element, rootElement);
+};
+
+setInterval(() => renderSeconds(), 1000);
