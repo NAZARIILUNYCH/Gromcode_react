4dc6b00a2574f59ab971386d4c2260dee8b4f7f0
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-01-26 14:21:44 +0200

    Todo List + JSX

---
+++ lesson2/hw1/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './list.css';
+
+const rootElement = document.querySelector('#root');
+const styles = { textAlign: 'center' };
+
+const element = (
+  <>
+    <h1 class="title" style={styles}>
+      Todo List
+    </h1>
+    <main class="todo-list">
+      <div class="actions">
+        <input class="task-input" type="text" />
+        <button class="btn create-task-btn">Create</button>
+      </div>
+      <ul class="list"></ul>
+    </main>
+  </>
+);
+
+ReactDOM.render(element, rootElement);
