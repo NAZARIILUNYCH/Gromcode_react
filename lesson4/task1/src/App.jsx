a6fb90c0254b0653bfd607e489b026bedcebf775
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-01-28 17:28:55 +0200

    Counter

--- lesson4/task1/src/App.jsx
+++ lesson4/task1/src/App.jsx
@@ -2,6 +2,12 @@
 import React from 'react';
 import Counter from './Counter';
 
-const App = () => <Counter />;
+const App = () => (
+  <>
+    <Counter start={17} interval={1000} />
+    <Counter start={-50} interval={100} />
+    <Counter start={12} interval={10000} />
+  </>
+);
 
 export default App;
