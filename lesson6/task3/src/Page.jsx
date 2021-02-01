87b753278ff872d3a70d238a7609cbd84eaea5cc
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-02-01 18:05:23 +0200

    Canceling rendering

---
+++ lesson6/task3/src/Page.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-unresolved */
+import React, { Component } from 'react';
+import Message from './Message';
+
+class Page extends Component {
+  state = {
+    text: null,
+  };
+
+  setText = text => {
+    this.setState({
+      text,
+    });
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <Message text={this.state.text} />
+        <div className="actions">
+          <button className="btn" onClick={() => this.setText('Hello, world!')}>
+            Text 1
+          </button>
+          <button className="btn" onClick={() => this.setText('Another exciting text.')}>
+            Text 2
+          </button>
+          <button className="btn" onClick={() => this.setText('')}>
+            Clear
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Page;
