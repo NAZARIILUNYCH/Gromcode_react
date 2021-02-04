2c80171480c8cc2511c47befa8c455232b425cca
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-02-04 15:20:41 +0200

    User form

---
+++ lesson9/task2/src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/no-unresolved */
+import React, { Component } from 'react';
+import UserForm from './UserForm';
+
+class App extends Component {
+  createUser = user => {
+    console.log(user);
+  };
+
+  render() {
+    return <UserForm onSubmit={this.createUser} />;
+  }
+}
+
+export default App;
