5733a7e157b3875b2e1d7259530bcef0bdd11134
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-02-04 16:26:54 +0200

    Uncontrolled component

---
+++ lesson9/task3/src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-unresolved */
+import React, { Component } from 'react';
+import UserForm from './UserForm';
+
+class App extends Component {
+  createUser = user => {
+    console.log(user);
+  };
+
+  render() {
+    return <UserForm onSubmit={this.createUser} />;
+  }
+}
+
+export default App;
