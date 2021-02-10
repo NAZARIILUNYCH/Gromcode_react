98ae17ea8200b5bee9dc593dfb17503febff0895
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-02-10 14:36:26 +0200

    Router

--- Live_coding/react_router/src/User.jsx
+++ Live_coding/react_router/src/User.jsx
@@ -1,17 +1,60 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { Component } from 'react';
 
-const User = () => (
-  <div className="user">
-    <img
-      alt="User Avatar"
-      src="https://avatars1.githubusercontent.com/u/9919?v=4"
-      className="user__avatar"
-    />
-    <div className="user__info">
-      <span className="user__name">GitHub</span>
-      <span className="user__location">San Francisco,CA</span>
-    </div>
-  </div>
-);
+class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userAvatar: null,
+      userName: null,
+      userLocation: null,
+    };
+  }
+
+  componentDidMount() {
+    this.getUserData(this.props.match.params.userId);
+  }
+
+  componentDidUpdate(prevProps) {
+    const curUserId = this.props.match.params.userId;
+    if (prevProps.match.params.userId !== curUserId) {
+      this.getUserData(curUserId);
+    }
+  }
+
+  getUserData = userId => {
+    // input: userId
+    // output: promise or undefined
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(response => response.json())
+      .then(userData => {
+        const { avatar_url, name, location } = userData;
+
+        this.setState({
+          userAvatar: avatar_url,
+          userName: name,
+          userLocation: location,
+        });
+      });
+    // TODO make error handling
+  };
+
+  render() {
+    const { userAvatar, userName, userLocation } = this.state;
+
+    if (!userAvatar || !userName || !userLocation) {
+      return null;
+    }
+    return (
+      <div className="user">
+        <img alt="User Avatar" src={userAvatar} className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">{userName}</span>
+          <span className="user__location">{userLocation}</span>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default User;
