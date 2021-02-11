b4b080266f5c614c01854960f0d9113853acba8e
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-02-11 11:28:15 +0200

    Window dimensions - React hooks

---
+++ lesson14/task3/src/App.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import Dimensions from './Dimensions';
+
+const App = () => <Dimensions />;
+
+export default App;
---
+++ lesson14/task3/src/Dimensions.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-shadow */
+import React, { useEffect, useState } from 'react';
+
+const Dimensions = () => {
+  const [demensions, setDimensions] = useState({
+    width: null,
+    height: null,
+  });
+
+  useEffect(() => {
+    const { innerWidth, innerHeight } = window;
+    setDimensions({ width: innerWidth, height: innerHeight });
+    const handleResize = e => {
+      const { innerWidth, innerHeight } = e.target;
+      setDimensions({ width: innerWidth, height: innerHeight });
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const { width, height } = demensions;
+
+  return <div>{`${width}px - ${height}px`}</div>;
+};
+
+export default Dimensions;
