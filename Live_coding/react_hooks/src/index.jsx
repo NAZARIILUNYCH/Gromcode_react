713ab253700101512f9d1f5c4f14db7cf9a48452
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-02-15 12:57:13 +0200

    FE-15

--- Live_coding/react_hooks/src/index.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-unresolved */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-
-const rootElement = document.querySelector('#root');
-
-ReactDOM.render(<App />, rootElement);
